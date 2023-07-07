b17d94826878434ea43487b44daa9240515618c8
Игорь Рябуша <igorman.2016@gmail.com>
2023-07-07 20:47:24 +0400

    home-page welcome-block about-us

--- SvbezDjangoWeb/store/db_handler.py
+++ SvbezDjangoWeb/store/db_handler.py
@@ -1,2 +1,9 @@
-from .models import *
+import os
+import hashlib
 
+
+def hash_upload(instance, filename):
+    instance.my_file.open()  # make sure we're at the beginning of the file
+    contents = instance.my_file.read()  # get the contents
+    fname, ext = os.path.splitext(filename)
+    return "{0}_{1}{2}".format(fname, hashlib.sha256().update(contents), ext)  # assemble the filename
